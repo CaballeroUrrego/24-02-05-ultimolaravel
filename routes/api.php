75f10a8c31058ca3a4b341ade3f9c8844e35080b
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2025-03-05 14:15:00 -0500

    Configuracion de ProductController.php

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 // Ruta para obtener el usuario autenticado utilizando Sanctum para la autenticación
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
